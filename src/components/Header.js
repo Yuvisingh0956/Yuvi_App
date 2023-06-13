d74af875a03b1899850785a71dbfeeafef9c55cd
Yuvisingh0956 <yvtokesh@gmail.com>
2023-06-13 18:27:06 +0530

    Add folder to repository

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+export const Header = () => {
+  return (
+    <header style={styles.header}>
+      <h1 style={styles.headerText}>Y's Drive</h1>
+      <p style={styles.headerSubText}>
+        A File Manager created by yuvi
+      </p>
+    </header>
+  );
+};
+
+const styles = {
+  header: {
+    backgroundColor: "#333",
+    color: "#fff",
+    textAlign: "center",
+    padding: "10px",
+  },
+  headerText: {
+    fontSize: "30px",
+  },
+  headerSubText: {
+    fontSize: "15px",
+  },
+};
